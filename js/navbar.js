dd6fa9914494085b33b1cfac0260f8788f416d79
William Viktorsson <william@viktorsson.com>
2021-04-20 16:48:07 +0200

    navigation source code changed to not lag

--- js/navbar.js
+++ js/navbar.js
@@ -30,6 +30,6 @@ function setCorrectNavBarItemAsActive() {
     let array = document.getElementById("myNavbar").children;
     for (let index = 0; index < array.length; index++) {
         const element = array[index];
-        element.className += (element.hasAttribute("href") && element.href.includes(location.pathname)) ? "active" : ""
+        element.className += (element.hasAttribute("href") && element.href.includes(location.pathname) && location.pathname.includes(".html")) ? "active" : ""
     }
 }
