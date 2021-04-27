78d54d223dab9f130658b156c9334c30065ffb2d
axelun <axelun10@edu.umea.se>
2021-04-27 14:17:15 +0200

    added gitignore

---
+++ js/clicker.js
@@ -0,0 +1,8 @@
+var adder = 1;
+var clicks = 0;
+function addBilar(){
+    clicks = clicks + adder
+    counter.innerHTML = clicks
+    document.getElementById("clickerGame").style.width = "200px"
+    console.log(document.getElementById("clickerGame"))
+}
